944a9efbc1b5cc64d00eb20ca92e0f0e0a57370e
root <root@homestead>
2024-03-03 16:11:58 +0000

    Fix bugs: Add proper parameter

---
+++ app/Constants/Constants.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Constants;
+
+/**
+ * Class Constants
+ * @package APP\Constants
+ */
+
+class Constants
+{
+    public const SPACE = ' ';
+    public const HYPHEN = '-';
+    public const DOT = '.';
+    public const ZERO = 0;
+    public const HALF = 0.5;
+    public const ONE = 1;
+    public const TWO = 2;
+    public const THREE = 3;
+    public const FOUR = 4;
+    public const FIVE = 5;
+    public const NINE = 9;
+    public const TEN = 10;
+    public const STATUS_TRUE = true;
+    public const STATUS_FALSE = false;
+}
